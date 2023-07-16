2910b4a5206f9ddc2b31fe558684f3c4b84bc513
ALİ DURNA <ali_d@Alis.bbrouter>
2023-07-16 11:04:33 +0300

    0.2

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 // -------------------------------------
 
 const app = express();
+dotenv.config();
+
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("Connected to DB");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
 
 app.listen(8800, () => {
+  connect();
   console.log("connecting !");
 });
+
 // -------------------------------------
