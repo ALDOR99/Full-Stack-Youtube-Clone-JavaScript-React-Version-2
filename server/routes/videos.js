efdbfa98840a82f0ce0d6aafc0a50e87c2f057ed
ALİ DURNA <ali_d@Alis.bbrouter>
2023-07-28 16:40:35 +0300

    0.8

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,7 +1,16 @@
 import express from "express";
-import {} from "../controllers/video.js";
+import { addVideo } from "../controllers/video.js";
 // ---------------------------------
 const router = express.Router();
 
+// create a video
+router.post("/", verifyToken, addVideo);
+router.put("/:id", verifyToken, addVideo);
+router.delete("/:id", verifyToken, addVideo);
+router.get("/find/:id", addVideo);
+router.put("/view/:id", addView);
+router.get("/trend", trend);
+router.get("/random", random);
+router.get("/sub", sub);
 // ---------------------------------
 export default router;
