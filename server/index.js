e9bb4f79eaab2c4e469efd53cc686319904d2f92
ALİ DURNA <ali_d@Alis.bbrouter>
2023-07-28 15:34:47 +0300

    0.8

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import userRoutes from "./routes/users.js";
 import videoRoutes from "./routes/users.js";
 import commentRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
-
+import cookieParser from "cookie-parser";
 // -------------------------------------
 
 const app = express();
@@ -22,10 +22,23 @@ const connect = () => {
       throw err;
     });
 };
+
+app.use(cookieParser());
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/videos", videoRoutes);
 app.use("/api/comments", commentRoutes);
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong!";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
 
 app.listen(8800, () => {
   connect();
