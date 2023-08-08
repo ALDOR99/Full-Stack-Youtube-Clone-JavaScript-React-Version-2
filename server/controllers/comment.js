0974973294f5add8cc5620af7c360be12ee0181f
ALİ DURNA <ali_d@Alis.bbrouter>
2023-08-08 15:42:03 +0300

    0.10

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,4 +1,4 @@
-import { createError } from "../error";
+import { createError } from "../error.js";
 // ----------------------------------------------------------------
 export const addComment = async (req, res, next) => {
   const newComment = new Comment({ ...req.body, userId: req.user.id });
@@ -29,6 +29,8 @@ export const deleteComment = async (req, res, next) => {
 
 export const getComments = async (req, res, next) => {
   try {
+    const comments = await Comment.find({ videoId: req.params.videoId });
+    res.status(200).json(comments);
   } catch (err) {
     next(next);
   }
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -79,12 +79,26 @@ export const unsubscribe = async (req, res, next) => {
 };
 
 export const like = async (req, res, next) => {
+  const id = req.user.id;
+  const videoId = req.user.videoId;
   try {
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { likes: id },
+      $pull: { dislikes: id },
+    });
+    res.status(200).json("The video has been liked.");
   } catch (err) {}
 };
 
 export const dislike = async (req, res, next) => {
+  const id = req.user.id;
+  const videoId = req.params.videoId;
   try {
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { dislikes: id },
+      $pull: { likes: id },
+    });
+    res.status(200).json("The video has been disliked.");
   } catch (err) {}
 };
 
